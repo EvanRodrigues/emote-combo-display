13b1441573788d872cd390cabed85272f8954db5
EvanRodrigues <evanthomasrodrigues@gmail.com>
2020-01-27 18:00:14 -0500

    Initial use of FFZ and BTTV apis

--- Emote.js
+++ Emote.js
@@ -1,4 +1,12 @@
-var Emote = function Emote(codes, art, audio){
+var NewEmote = function Emote(code, art) {
+    this.code = code;
+    this.art = art;
+    this.combo = 0;
+    this.users = [];
+    this.timer = null;
+}
+
+var Emote = function Emote(codes, art, audio) {
     this.codes = codes;
     this.art = art;
     this.audio = audio;
